b1db8464d9b5bb2a3a5224f9bf25d70206457b4a
Trong Binh <trongbinh12366@gmail.com>
2022-09-06 17:18:52 +0700

    9_6_2022

---
+++ assets/js/slick_slider.js
@@ -0,0 +1,18 @@
+$('.slider-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    autoplaySpeed: 3000,
+    infinity: true,
+    autoplay: false,
+  });
+
+
+  $('.slider-nav-top').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    autoplaySpeed: 5000,
+    infinity: true,
+    autoplay: true,
+  });
